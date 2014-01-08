75f6a0a5081fa80e7a3ea3b9119b23396698fd0a
magnus <magnus@md-produktion.se>
2014-01-08 22:00:05 +0100

    Ändrat i backupskriptet

--- lab5/uppgift5.sh
+++ lab5/uppgift5.sh
@@ -14,9 +14,12 @@
 #
 Currentd=$(date +"%Y-%m-%d-%H.%M.%S")
 #
-Dirpath=testmappen # Variabel för källmappen 
+Dirpath=/home/magnus/lx13mappen/testmappen # Variabel för källmappen 
 #
 Backupath=$HOME/backup # Destinationsmappen
+
+Dir=`basename $Dirpath` # Ommodifiering där $Dirpath tilldelas till $Dir
+# och med hjälp av basename skalas överkataloger bort i sökvägen.
 #
 # Här testas  utifall om det finns ngn backupkatalog
 # Om den inte finns, skapas den.
@@ -28,12 +31,12 @@ Backupath=$HOME/backup # Destinationsmappen
 # Annars om det finns en läsbar katalog så komprimeras den
 # och flyttas till destinationsmappen/backuppmappen.
 #
-if [ ! -r "$Dirpath" ];then
+if [ ! -r "$Dir" ];then
 echo  "Mappen är ej läsbar."
-   elif [ -d "$Dirpath" ];then
-	$(tar --recursion -czf  backupfile.tar.gz $Dirpath)
+   elif [ -d "$Dir" ];then
+	$(tar --recursion -czf  backupfile.tar.gz $Dir)
 	mv backupfile.tar.gz $Backupath/backupfile_$Currentd.tar.gz
-	echo "En Backup har gjorts av $Dirpath."
+	echo "En Backup har gjorts av $Dir."
    else echo "Någonting gick fel."	#Felmeddelande
    fi
 exit 0 #Skriptet avslutas.
